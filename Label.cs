42cb6e09bcc2ca8695a30aab58122f3602c5fb81
deaddog <mikkelslsl@gmail.com>
2016-02-27 15:55:00 +0100

    Created an abstract Label class, that will be used as the base for a composite structure.

---
+++ Label.cs
@@ -0,0 +1,9 @@
+﻿namespace DLM.Inference
+{
+    /// <summary>
+    /// Defines the top-level type of the label composite structure.
+    /// </summary>
+    public abstract class Label
+    {
+    }
+}
