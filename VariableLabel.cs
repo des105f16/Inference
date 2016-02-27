5fc8068a10a89a5ba2a08c69cab528e2eaa02ade
deaddog <mikkelslsl@gmail.com>
2016-02-27 17:17:42 +0100

    Added a current upper bound to the variable label.

--- VariableLabel.cs
+++ VariableLabel.cs
@@ -8,6 +8,7 @@ namespace DLM.Inference
     public class VariableLabel : Label
     {
         private string name;
+        private Label currentUpperBound;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="VariableLabel"/> class.
@@ -23,12 +24,27 @@ namespace DLM.Inference
                 throw new ArgumentException("The name of a label cannot be the empty string.", nameof(name));
 
             this.name = name;
+            this.currentUpperBound = UpperBoundLabel.Singleton;
         }
 
         /// <summary>
         /// Gets the name associated with the label.
         /// </summary>
         public string Name => name;
+        /// <summary>
+        /// Gets or sets the current upper bound of this variable label.
+        /// </summary>
+        public Label CurrentUpperBound
+        {
+            get { return currentUpperBound; }
+            set
+            {
+                if (value == null)
+                    throw new ArgumentNullException(nameof(value));
+
+                currentUpperBound = value;
+            }
+        }
 
         /// <summary>
         /// Returns a <see cref="string"/> that represents this <see cref="VariableLabel"/>.
