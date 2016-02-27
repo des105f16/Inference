74ad70067e4d4f9346aae2eda0c1ab572a3f266a
deaddog <mikkelslsl@gmail.com>
2016-02-27 16:04:57 +0100

    Created a helper class for providing simplified string joins in ToString implementations.

---
+++ StringJoinExtension.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DLM.Inference
+{
+    internal static class StringJoinExtension
+    {
+        public static string JoinString<T>(this IEnumerable<T> collection, string separator)
+        {
+            return string.Join(separator, collection.Select(x => x.ToString()));
+        }
+    }
+}
