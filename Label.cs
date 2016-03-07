d8178c97a66c64a2b3bb27b116e59c7f9b0e885b
deaddog <mikkelslsl@gmail.com>
2016-03-07 21:17:35 +0100

    Added a better message for the NotImplementedExceptions thrown by LessRestrictiveThan methods.

--- Label.cs
+++ Label.cs
@@ -69,25 +69,27 @@ namespace DLM.Inference
 
         protected internal virtual bool LessRestrictiveThan(PolicyLabel label)
         {
-            throw new NotImplementedException();
+            throw new NotImplementedException(restrictiveNotImplemented(label));
         }
         protected internal virtual bool LessRestrictiveThan(ConstantLabel label)
         {
-            throw new NotImplementedException();
+            throw new NotImplementedException(restrictiveNotImplemented(label));
         }
         protected internal bool LessRestrictiveThan(VariableLabel label)
         {
-            throw new NotImplementedException();
+            throw new NotImplementedException(restrictiveNotImplemented(label));
         }
 
         protected internal virtual bool LessRestrictiveThan(JoinLabel label)
         {
-            throw new NotImplementedException();
+            throw new NotImplementedException(restrictiveNotImplemented(label));
         }
         protected internal virtual bool LessRestrictiveThan(MeetLabel label)
         {
-            throw new NotImplementedException();
+            throw new NotImplementedException(restrictiveNotImplemented(label));
         }
+
+        private string restrictiveNotImplemented(Label label) => $"There is now implementation for {GetType().Name} \u2291 {label.GetType().Name}.";
 
         protected internal bool LessRestrictiveThan(LowerBoundLabel label) => this is LowerBoundLabel;
         protected internal bool LessRestrictiveThan(UpperBoundLabel label) => true;
