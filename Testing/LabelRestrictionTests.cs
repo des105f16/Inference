2ac5637527d930d6222e37c35ea9313e70848e64
deaddog <mikkelslsl@gmail.com>
2016-03-22 13:20:07 +0100

    Created tests for upper- and lower-bound labels.

--- Testing/LabelRestrictionTests.cs
+++ Testing/LabelRestrictionTests.cs
@@ -37,5 +37,41 @@ namespace DLM.Inference.Tests
             v1 = new VariableLabel(nameof(v1));
             v2 = new VariableLabel(nameof(v2));
         }
+
+        [TestMethod()]
+        public void UpperBound()
+        {
+            Assert.IsFalse(Label.UpperBound <= p1);
+            Assert.IsTrue(p1 <= Label.UpperBound);
+            Assert.IsFalse(Label.UpperBound <= p2);
+            Assert.IsTrue(p2 <= Label.UpperBound);
+
+            Assert.IsFalse(Label.UpperBound <= c1);
+            Assert.IsTrue(c1 <= Label.UpperBound);
+            Assert.IsFalse(Label.UpperBound <= c2);
+            Assert.IsTrue(c2 <= Label.UpperBound);
+
+            Assert.IsFalse(Label.UpperBound <= Label.LowerBound);
+            Assert.IsTrue(Label.LowerBound <= Label.UpperBound);
+            Assert.IsTrue(Label.UpperBound <= Label.UpperBound);
+        }
+
+        [TestMethod()]
+        public void LowerBound()
+        {
+            Assert.IsTrue(Label.LowerBound <= p1);
+            Assert.IsFalse(p1 <= Label.LowerBound);
+            Assert.IsTrue(Label.LowerBound <= p2);
+            Assert.IsFalse(p2 <= Label.LowerBound);
+
+            Assert.IsTrue(Label.LowerBound <= c1);
+            Assert.IsFalse(c1 <= Label.LowerBound);
+            Assert.IsTrue(Label.LowerBound <= c2);
+            Assert.IsFalse(c2 <= Label.LowerBound);
+
+            Assert.IsTrue(Label.LowerBound <= Label.UpperBound);
+            Assert.IsFalse(Label.UpperBound <= Label.LowerBound);
+            Assert.IsTrue(Label.LowerBound <= Label.LowerBound);
+        }
     }
 }
