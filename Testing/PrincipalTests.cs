aa28196c71901da036043c9347f0a1b2ffff94d4
mikaelec <mikaelec@gmail.com>
2016-03-24 12:49:55 +0100

    Added empty tests for AddSubordinate and ActualSubordinates.

---
+++ Testing/PrincipalTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using DLM.Inference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLM.Inference.Tests
+{
+    [TestFixture()]
+    public class PrincipalTests
+    {
+        [Test()]
+        public void AddSubordinateTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void ActualSubordinatesTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
