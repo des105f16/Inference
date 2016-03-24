d569fc8682af089a7cb65edfbf5ff6c21db1201c
mikaelec <mikaelec@gmail.com>
2016-03-24 13:00:43 +0100

    Added test initialization with principals from Myers2000 Fig. 3.

--- Testing/PrincipalTests.cs
+++ Testing/PrincipalTests.cs
@@ -11,6 +11,19 @@ namespace DLM.Inference.Tests
     [TestFixture()]
     public class PrincipalTests
     {
+        private Principal carl, manager, doctor, bob, amy, group;
+
+        [SetUp()]
+        public void Initialize()
+        {
+            carl = new Principal(nameof(carl));
+            manager = new Principal(nameof(manager));
+            doctor = new Principal(nameof(doctor));
+            bob = new Principal(nameof(bob));
+            amy = new Principal(nameof(amy));
+            group = new Principal(nameof(group));
+        }
+
         [Test()]
         public void AddSubordinateTest()
         {
