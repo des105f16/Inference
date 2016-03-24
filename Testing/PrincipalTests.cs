557290a4a86b351c04fcf3180c1c299ce0f43e5b
mikaelec <mikaelec@gmail.com>
2016-03-24 13:02:28 +0100

    Added two more principals for AddSubordinate test.

--- Testing/PrincipalTests.cs
+++ Testing/PrincipalTests.cs
@@ -11,11 +11,15 @@ namespace DLM.Inference.Tests
     [TestFixture()]
     public class PrincipalTests
     {
+        private Principal p1, p2;
         private Principal carl, manager, doctor, bob, amy, group;
 
         [SetUp()]
         public void Initialize()
         {
+            p1 = new Principal(nameof(p1));
+            p2 = new Principal(nameof(p2));
+
             carl = new Principal(nameof(carl));
             manager = new Principal(nameof(manager));
             doctor = new Principal(nameof(doctor));
